1891db34c8a53b69234996605d60e0e8bba53c6c
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-18 17:09:12 -0500

    Add functionality to remove books form Array and update ids

--- script.js
+++ script.js
@@ -6,21 +6,30 @@ const bookList = document.querySelector(".book-list");
 let booksArray = [];
 
 function addBook() {
+    booksArray.push({title: titleInput.value, author: authorInput.value, id: booksArray.length});
+
   const book = `
     <div class="book">
         <h4 class="book-title">${titleInput.value}</h4>
         <h4 class="book-author">${authorInput.value}</h4>
+        <h4 class="book-id">${booksArray[booksArray.length - 1]['id']}</h4>
         <button class="remove-btn" onclick="removeBookFromList(this)">Remove</button>
     </div>`;
 
   bookList.insertAdjacentHTML("beforeend", book);
 
-  booksArray.push({title: titleInput.value, author: authorInput.value, id: booksArray.length});
   console.log(booksArray);
 }
 
 addButton.addEventListener("click", addBook);
 
-function removeBookFromList(e) {
-  e.parentElement.remove();  
+function removeBookFromList(e) {    
+    booksArray.splice(+e.parentElement.children[2].innerHTML, 1);
+
+    e.parentElement.remove();
+
+    booksArray.forEach((book, i) => {
+        book.id = i;
+        bookList.children[i].children[2].innerHTML = i;
+    });
 }
