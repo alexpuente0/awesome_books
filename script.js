d7a7317c115a59ef4f5307648455e80c69e18536
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-18 15:01:35 -0500

    Create addBook() function

---
+++ script.js
@@ -0,0 +1,17 @@
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addButton = document.getElementById('button');
+const bookList = document.querySelector('.book-list');
+
+function addBook() {
+    const book = `
+    <div class="book">
+        <h4 class="book-title">${titleInput.value}</h4>
+        <h4 class="book-author">${authorInput.value}</h4>
+        <button class="remove-btn">Remove</button>
+    </div>`;
+
+    bookList.insertAdjacentHTML("beforeend", book);
+}
+
+addButton.addEventListener('click' ,addBook);
