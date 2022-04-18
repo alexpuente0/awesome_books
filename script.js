62f657ef56c1ce1e7536e6f8f878f94f3a4b6c50
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-18 18:56:39 -0500

    Add functionality to load books form localStorage

--- script.js
+++ script.js
@@ -5,29 +5,27 @@ const bookList = document.querySelector(".book-list");
 
 let booksArray = [];
 
+function renderBook(title, author, id) {
+    const book = `
+      <div class="book">
+          <h4 class="book-title">${title}</h4>
+          <h4 class="book-author">${author}</h4>
+          <h4 class="book-id">${id}</h4>
+          <button class="remove-btn" onclick="removeBook(this)">Remove</button>
+      </div>`;
+  
+    bookList.insertAdjacentHTML("beforeend", book);
+  }
+
 function addBook() {
     booksArray.push({title: titleInput.value, author: authorInput.value, id: booksArray.length});
+    localStorage.setItem("bookdata", JSON.stringify(booksArray));
 
-  const book = `
-    <div class="book">
-        <h4 class="book-title">${titleInput.value}</h4>
-        <h4 class="book-author">${authorInput.value}</h4>
-        <h4 class="book-id">${booksArray[booksArray.length - 1]['id']}</h4>
-        <button class="remove-btn" onclick="removeBookFromList(this)">Remove</button>
-    </div>`;
-
-  bookList.insertAdjacentHTML("beforeend", book);
-
-  console.log(booksArray);
-
-  localStorage.setItem("bookdata", JSON.stringify(booksArray));
-
+    renderBook(titleInput.value, authorInput.value, booksArray.length);
 }
 
-addButton.addEventListener("click", addBook);
-
-function removeBookFromList(e) {    
-    booksArray.splice(+e.parentElement.children[2].innerHTML, 1);
+function removeBook(e) {    
+    booksArray.splice(e.parentElement.children[2].innerHTML, 1);
 
     e.parentElement.remove();
 
@@ -37,26 +35,14 @@ function removeBookFromList(e) {
     });
 
       localStorage.setItem("bookdata", JSON.stringify(booksArray));
-
-}
-
-function renderBooks(title, author, id) {
-  const book = `
-    <div class="book">
-        <h4 class="book-title">${title}</h4>
-        <h4 class="book-author">${author}</h4>
-        <h4 class="book-id">${id}</h4>
-        <button class="remove-btn" onclick="removeBookFromList(this)">Remove</button>
-    </div>`;
-
-  bookList.insertAdjacentHTML("beforeend", book);
 }
 
 function loadBooks() {
-  booksArray = localStorage.getItem(JSON.parse("bookdata"))
+  booksArray = JSON.parse(localStorage.getItem('bookdata'))
   booksArray.forEach((book) => {
-renderBooks(book.title, book.author, book.id);
-  })
+    renderBook(book.title, book.author, book.id);
+  });
 }
 
-
+loadBooks();
+addButton.addEventListener("click", addBook);
