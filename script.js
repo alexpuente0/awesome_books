6dd2d2cf64e1a129a5494cdf29116c5ea8a4efbb
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-04-18 15:29:20 -0500

    Project advancement

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ const authorInput = document.getElementById('author');
 const addButton = document.getElementById('button');
 const bookList = document.querySelector('.book-list');
 
+
 function addBook() {
     const book = `
-    <div class="book">
+    <div class="book${x}">
         <h4 class="book-title">${titleInput.value}</h4>
         <h4 class="book-author">${authorInput.value}</h4>
         <button class="remove-btn">Remove</button>
     </div>`;
 
+  
     bookList.insertAdjacentHTML("beforeend", book);
+      const removebook = document.getElementsByClassName("remove-btn"); 
+removebook.addEventListener("click", deleteBook);
+    
 }
 
 addButton.addEventListener('click' ,addBook);
+
+
+function deleteBook() {
+  this.parentElement.remove()
+}
+
