16b8e11cd1a09e389842aac4510a42acc7ce6a49
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-04-19 12:17:41 -0500

    Refactor code to use Classes

--- script.js
+++ script.js
@@ -7,6 +7,85 @@ let booksArray = [];
 
 /* eslint-disable no-unused-vars */
 
+class BookEntry {
+  constructor(title, author, idNum) {
+    this.title = title;
+    this.author = author;
+    this.idNum = idNum;
+  }
+
+  add() {
+    function creaateAndSet(title, author, idNum) {
+      const bookBlock = document.createElement('div');
+      const bookInfo = document.createElement('div');
+      const bookTitle = document.createElement('h4');
+      const bookAuthor = document.createElement('h4');
+      const bookIdNumber = document.createElement('h4');
+      const removeButton = document.createElement('button');
+      bookTitle.innerHTML = `${title}`;
+      bookAuthor.innerHTML = `by ${author}`;
+      bookIdNumber.innerHTML = idNum;
+      removeButton.innerHTML = 'Remove';
+
+      bookInfo.append(bookTitle, bookAuthor, bookIdNumber);
+      bookBlock.append(bookInfo, removeButton);
+      bookList.append(bookBlock);
+
+      BookEntry.remove(removeButton);
+    }
+    creaateAndSet(this.title, this.author, this.id);
+  }
+
+  static remove(element) {
+    function removeBook() {
+      booksArray = booksArray.filter(
+        (book) =>
+          +book.id !== +this.parentNode.children[0].children[2].innerHTML
+      );
+      this.parentNode.remove();
+
+      booksArray.forEach((book, i) => {
+        bookList.children[i].children[0].children[2].innerHTML = i;
+        book.idNum = i;
+      });
+      localStorage.setItem('bookdata', JSON.stringify(booksArray));
+    }
+    element.addEventListener('click', removeBook);
+  }
+
+  static addBooks() {
+    if (titleInput.value !== '' && authorInput.value !== '') {
+      const newBook = new BookEntry(
+        titleInput.value,
+        authorInput.value,
+        booksArray.length
+      );
+      newBook.add();
+      booksArray.push(newBook);
+
+      localStorage.setItem('bookdata', JSON.stringify(booksArray));
+
+      titleInput.value = '';
+      authorInput.value = '';
+    }
+  }
+
+  static loadBooks() {
+    JSON.parse(localStorage.getItem('bookdata')).forEach((book, i) => {
+      const newBook = new BookEntry(book.title, book.author, i);
+      newBook.add();
+      booksArray.push(newBook);
+    });
+  }
+}
+
+addButton.addEventListener('click', BookEntry.addBooks);
+
+BookEntry.loadBooks();
+
+// The Previous iteration of the code
+
+/* 
 function removeBook(e) {
   booksArray.splice(e.parentElement.children[2].innerHTML, 1);
 
@@ -59,4 +138,4 @@ function addBook() {
 }
 
 addButton.addEventListener('click', addBook);
-loadBooks();
+loadBooks(); */
