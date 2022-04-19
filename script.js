57af0a96fd08a34ac1e8421884a1b2b3a3c6f6e8
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-18 19:52:02 -0500

    Fix linter errors

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const addButton = document.getElementById("button");
-const bookList = document.querySelector(".book-list");
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addButton = document.getElementById('button');
+const bookList = document.querySelector('.book-list');
 
 let booksArray = [];
 
+function removeBook(e) {
+  booksArray.splice(e.parentElement.children[2].innerHTML, 1);
+
+  e.parentElement.remove();
+
+  booksArray.forEach((book, i) => {
+    book.id = i;
+    bookList.children[i].children[2].innerHTML = i;
+  });
+
+  localStorage.setItem('bookdata', JSON.stringify(booksArray));
+}
+
 function renderBook(title, author, id) {
-    const book = `
+  const book = `
       <div class="book">
           <h4 class="book-title">${title}</h4>
           <h4 class="book-author">${author}</h4>
           <h4 class="book-id">${id}</h4>
-          <button class="remove-btn" onclick="removeBook(this)">Remove</button>
+          <button class="remove-btn" onclick="${removeBook(this)}">Remove</button>
       </div>`;
-  
-    bookList.insertAdjacentHTML("beforeend", book);
-  }
 
-function addBook() {
-    booksArray.push({title: titleInput.value, author: authorInput.value, id: booksArray.length});
-    localStorage.setItem("bookdata", JSON.stringify(booksArray));
-
-    renderBook(titleInput.value, authorInput.value, booksArray.length);
+  bookList.insertAdjacentHTML('beforeend', book);
 }
 
-function removeBook(e) {    
-    booksArray.splice(e.parentElement.children[2].innerHTML, 1);
+function addBook() {
+  booksArray.push({ title: titleInput.value, author: authorInput.value, id: booksArray.length });
+  localStorage.setItem('bookdata', JSON.stringify(booksArray));
 
-    e.parentElement.remove();
-
-    booksArray.forEach((book, i) => {
-        book.id = i;
-        bookList.children[i].children[2].innerHTML = i;
-    });
-
-      localStorage.setItem("bookdata", JSON.stringify(booksArray));
+  renderBook(titleInput.value, authorInput.value, booksArray.length);
 }
 
 function loadBooks() {
-  booksArray = JSON.parse(localStorage.getItem('bookdata'))
+  booksArray = JSON.parse(localStorage.getItem('bookdata'));
   booksArray.forEach((book) => {
     renderBook(book.title, book.author, book.id);
   });
 }
 
 loadBooks();
-addButton.addEventListener("click", addBook);
+addButton.addEventListener('click', addBook);
